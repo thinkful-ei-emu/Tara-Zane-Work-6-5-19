edb660dc6d47b9b72f7639b76658ae90ad8117e3
Zane Kuehn <zanekuehn@yahoo.com>
2019-06-05 11:40:41 -0500

    Completes Enroll In Summer School Function

--- drills2.js
+++ drills2.js
@@ -3,8 +3,8 @@
 function makeStudentsReport(data) {
   let result = [];
   data.forEach(function(student) {
-    result.push(`${student.name}: ${student.grade}`)
-  })
+    result.push(`${student.name}: ${student.grade}`);
+  });
   return result;
 }
 
@@ -15,3 +15,32 @@ const testData = [
 ];
 
 console.log(makeStudentsReport(testData));
+
+
+function enrollInSummerSchool(students){
+  
+  students.forEach(function(obj){
+    obj.status='In Summer School'
+  });
+  return students;
+}
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+
+
+console.log(enrollInSummerSchool(studentData));
