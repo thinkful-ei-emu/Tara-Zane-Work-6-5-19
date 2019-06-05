c34e38ebf2807b9260821fe311a1c5cab3700489
Tara Patel <t.jeanne.patel@gmail.com>
2019-06-05 13:38:16 -0500

    added loaf object

--- drills2.js
+++ drills2.js
@@ -102,3 +102,14 @@ const objectB = {
 const testKeys = ['id', 'name', 'age', 'city'];
 
 console.log(validateKeys(objectB,testKeys));
+
+const loaf = {
+  flour: 300,
+  water: 210,
+  hydration: function() {
+    return this.water/this.flour*100;
+  }
+};
+
+console.log(loaf.flour, loaf.water);
+console.log(loaf.hydration());
